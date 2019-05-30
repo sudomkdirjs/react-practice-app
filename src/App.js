f97f77bbffb68843a576b2837cd24692c1ca34b3
Marees <mareeswaran@ooyala.com>
2019-05-31 01:11:30 +0530

    Adds useState hook sample

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import StateHook from './components/StateHook';
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +21,10 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <main>
+        <StateHook />
+      </main>
     </div>
   );
 }
